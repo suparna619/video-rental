5a3025a68d81ae925d84bb9a4213e3ff81f2540b
Suparna <suparnad@thoughtworks.com>
2015-03-19 11:30:50 +0530

    HtmlStatement.class created for creating html statements

---
+++ src/com/twu/refactor/HtmlStatement.java
@@ -0,0 +1,14 @@
+package com.twu.refactor;
+
+/**
+ * Created by suparnad on 3/19/2015.
+ */
+public class HtmlStatement {
+    public static String htmlStatement() {
+        return "<H1>Rentals for <EM>Dinsdale Pirhana</EM></H1>" +
+                "<P>Monty Python and the Holy Grail: 3.5<BR>Ran: 2.0<BR>LA Confidential: 6.0" +
+                "<BR>Star Trek 13.2: 3.0<BR>Wallace and Gromit: 6.0" +
+                "<BR><P>You owe <EM>20.5</EM>" +
+                "<P>On this rental you earned <EM>6</EM> frequent renter points<P>\n";
+    }
+}
